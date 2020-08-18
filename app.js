6fa90f0d7017d1cbe444715e56a68212337cdb2c
axl <as090@mail.ru>
2020-08-19 01:46:26 +0300

    Добавлена отправка уведомления в telegram

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var ping = require('ping');
-var email = require('emailjs');
+const ping = require('ping');
+const email = require('emailjs');
 const dotenv = require('dotenv');
+const http = require('request');
 dotenv.config();
 
 function Pingolier(){
     this.options = {
         host: process.env.HOST,
-        interval: 300000
+        interval: 300000, //время повторного пинга, мс (5 мин)
+        errInterval: 30000 //время повторного пинга при недоступности хоста, мс (30 сек)
     };
 
     this.serverOptions = {
@@ -15,18 +17,24 @@ function Pingolier(){
         host: "smtp.yandex.ru",
         ssl: true
     };
+
+    this.err = false;
 };
 
 /**
  * Пингуем
  */
 Pingolier.prototype.ping = function (){
-    let _this = this;
+    const _this = this;
     ping.sys.probe(this.options.host, function(res){
         if(!res){
             _this.sendEmail();
+            _this.sendTelegram();
+            _this.err = true;
+            _this.timeOut();
         } else {
             console.log('хост доступен: ' + Date('now'));
+            _this.err = false;
             _this.timeOut();
         }
     });
@@ -36,8 +44,6 @@ Pingolier.prototype.ping = function (){
  * Отправляем сообщение
  */
 Pingolier.prototype.sendEmail = function () {
-    let _this = this;
-  
     let server = email.server.connect({
         user: this.serverOptions.user,
         password: this.serverOptions.password,
@@ -46,20 +52,32 @@ Pingolier.prototype.sendEmail = function () {
     });
   
     server.send({
-        text:    "Lavka отвалилась",
-        from:    "Lavka-Serv <" + this.serverOptions.user + ">",
+        text:    "Хост недоступен: "  + this.options.host,
+        from:    "Pingolier_bot <" + this.serverOptions.user + ">",
         to:      "Me <" + this.serverOptions.user + ">",
-        subject: "Lavka отвалилась"
-    }, function () {
-        _this.timeOut();
+        subject: "Хост недоступен: " + this.options.host
     });
-  };
+};
+
+/**
+ * Отправляем сообщение в телеграм
+ */
+Pingolier.prototype.sendTelegram = function(){
+    msg = encodeURI('Хост недоступен: ' + this.options.host);
+
+    http.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${process.env.GROUP}&parse_mode=html&text=${msg}`, 
+        function (error, response, body) {  
+            console.log('error:', error); 
+            console.log('statusCode:', response && response.statusCode); 
+            console.log('body:', body); 
+  });
+};
 
 /**
  * Ждем
  */
 Pingolier.prototype.timeOut = function () {
-    setTimeout(() => this.ping(), this.options.interval);
+    setTimeout(() => this.ping(), !this.err ? this.options.interval : this.options.errInterval);
 };
 
 let pingolier = new Pingolier();
